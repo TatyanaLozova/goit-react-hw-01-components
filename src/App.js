a975b24fae53fb86a85da017b3022433932dac33
Tatyana Loza <fly220@ukr.net>
2021-02-22 23:27:46 +0300

    +

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Profile from "./components/Profile.js"
+
+import user from './user.json'
+
+
+
+
+ const App = () => {
+   return (
+     <div>
+       <Profile {Profile} />
+     </div>
+   )
+   
+ }
+
+
+  export default App;
